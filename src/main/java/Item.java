d36c6da9f731438d2d55a890b171adcef353df9a
limelier <tudor.iacobescu.1@gmail.com>
2020-03-02 16:20:03 +0200

    Added Item interface.

---
+++ src/main/java/Item.java
@@ -0,0 +1,11 @@
+/**
+ * @author Iacobescu Tudor
+ */
+public interface Item {
+    /**
+     * Gets the profit factor, usually the value-to-weight ratio, of this item.
+     *
+     * @return the profit factor
+     */
+    public double profitFactor();
+}
