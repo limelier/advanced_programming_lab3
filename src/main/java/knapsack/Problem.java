23c43e830481144a3de30d3d7f604370c39be486
limelier <tudor.iacobescu.1@gmail.com>
2020-03-07 19:01:08 +0200

    Added a second constructor to Problem, which takes a list of items.

--- src/main/java/knapsack/Problem.java
+++ src/main/java/knapsack/Problem.java
@@ -11,16 +11,26 @@ public class Problem {
     private List<Item> itemList;
 
     /**
-     * Create a knapsack problem, with a knapsack of the given capacity and a list of items, sorted by name.
+     * Create a knapsack problem, with a knapsack of the given capacity and several items.
      *
      * @param capacity the capacity of the knapsack
-     * @param items the items presented by the problem
+     * @param items the items presented by the problem, which get sorted by name
      */
     public Problem(int capacity, Item ...items) {
         this.capacity = capacity;
         List<Item> itemList = Arrays.asList(items);
         itemList.sort(Comparator.comparing(Item::getName));
         this.itemList = itemList;
+    }
+
+    /**
+     * Create a knapsack problem, with a knapsack of the given capacity and a list of items.
+     *
+     * @param capacity the capacity of the knapsack
+     * @param items the items presented by the problem, which get sorted by name
+     */
+    public Problem(int capacity, List<Item> items) {
+        this(capacity, items.toArray(new Item[0]));
     }
 
     public int getCapacity() {
