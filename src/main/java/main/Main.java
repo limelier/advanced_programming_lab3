bf01d2df1cce808d624c330caf3fdd7a110fbe43
limelier <tudor.iacobescu.1@gmail.com>
2020-03-07 19:01:33 +0200

    Added a Problem generator, and a test for it.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,40 +5,17 @@ import algorithm.DynamicProgramming;
 import algorithm.GreedyApproximation;
 import item.*;
 import knapsack.Problem;
+import testing.Generator;
 
 /**
  * @author Iacobescu Tudor
  */
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Dragon Rising", 300, 5); // PF 1.66
-        Book book2 = new Book("A Blade in the Dark", 300, 5); // PF 1.66
-        Food food1 = new Food("Cabbage", 2); // PF 2
-        Food food2 = new Food("Rabbit", 2); // PF 2
-        Weapon weapon = new Weapon(Weapon.Material.Iron, Weapon.Type.Sword, 10, 5); // PF 2
-
-        Problem problem = new Problem(10, book1, book2, food1, food2, weapon);
+        Problem problem = Generator.generateProblem(10, 10);
         System.out.println(problem);
-
-        Algorithm alg = new GreedyApproximation(problem);
-        Algorithm alg2 = new DynamicProgramming(problem);
-        alg.solve();
-        System.out.println("Greedy algorithm solution:\n" + alg.result());
-        alg2.solve();
-        System.out.println("Dynamic programming solution:\n" + alg2.result());
-
-        // more interesting example:
-        System.out.println("\nSecond problem:");
-        Weapon item1 = new Weapon(Weapon.Material.Diamond, Weapon.Type.Sword, 10, 4); // PF 2.5
-        Weapon item2 = new Weapon(Weapon.Material.Stone, Weapon.Type.Axe, 10, 4); // PF 2.5
-        Weapon item3 = new Weapon(Weapon.Material.Netherite, Weapon.Type.Pickaxe, 12, 6); // PF 2
-        Problem problem2 = new Problem(10, item1, item2, item3);
-        System.out.println(problem2);
-        Algorithm alg3 = new GreedyApproximation(problem2);
-        alg3.solve();
-        System.out.println("Greedy algorithm solution:\n" + alg3.result());
-        Algorithm alg4 = new DynamicProgramming(problem2);
-        alg4.solve();
-        System.out.println("Dynamic programming solution:\n" + alg4.result());
+        Algorithm alg1 = new DynamicProgramming(problem);
+        alg1.solve();
+        System.out.println(alg1.result());
     }
 }
--- src/main/java/testing/Generator.java
+++ src/main/java/testing/Generator.java
@@ -5,19 +5,13 @@ import knapsack.Problem;
 
 import java.util.*;
 
-public class ProblemGenerator {
-
-
-    private static Item pickRandomItem() {
-        int category = ProblemGeneratorConstants.randomCategory();
-        switch (category) {
-        }
-    }
-
-    static Problem generateProblem(int items, int capacity) {
+public class Generator {
+    public static Problem generateProblem(int items, int capacity) {
         List<Item> itemList = new ArrayList<>(items);
         for (int i = 0; i < items; i++) {
-            itemList.add(pickRandomItem());
+            itemList.add(Utils.randomItem());
         }
+
+        return new Problem(capacity, itemList);
     }
 }
